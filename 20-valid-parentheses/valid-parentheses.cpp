557cc7a14d59c28c2e76f537b7583d98c76aa805
shivsins <shivjeetsingh3717@gmail.com>
2024-04-30 14:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (31.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,33 @@
+#include<stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c: s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }else{
+                if(!st.empty()){
+                    switch (c){
+                        case ')': 
+                            if(st.top()=='(') st.pop();
+                            else return false;
+                            break;
+                        case '}':
+                            if(st.top()=='{')st.pop();
+                            else return false;
+                            break;
+                        case ']':
+                            if(st.top()=='[') st.pop();
+                            else return false;
+                            break;
+                    }
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return st.empty();
+    }
+};
