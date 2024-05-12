2d57e177b20070f6eacf7336c10f9351bd66a4c9
shivsins <shivjeetsingh3717@gmail.com>
2024-05-12 16:35:48 +0530

    Time: 32 ms (55.93%) | Memory: 21.3 MB (91.07%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int m = INT_MIN;
+        int n = piles.size();
+        for(int i=0;i<n;i++){
+            m = max(m,piles[i]);
+        }
+        int l = 1, r = m;
+        while(l<r){
+            int mid = (l+r)/2;
+            int time = 0;
+            for(int i=0;i<n;i++){
+                if(piles[i]%mid==0) time+=piles[i]/mid;
+                else time+=(piles[i]/mid) + 1;
+            }
+            if(time>h) l=mid+1;
+            else r = mid;
+        }
+        return l;
+    }
+};
