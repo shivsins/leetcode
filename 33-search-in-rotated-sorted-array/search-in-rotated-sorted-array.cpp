daf18f299d816e4485ca1369db054bd99870ced1
shivsins <shivjeetsingh3717@gmail.com>
2024-05-15 17:30:16 +0530

    Time: 4 ms (41.87%) | Memory: 13.7 MB (5.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,3 +1,9 @@
+//two ways to solve it, one is to find the minimum value index and then treat the problem as two differnt sorted arrays.
+// other one is a little different and it does it in a single go which is what I have coded below.
+// so we know there exists two sorted portions in the same array. left and right sorted portions.
+// so with each iteration we check which portion we are in
+// then we check where the target lies, and we make a move accordingly
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
