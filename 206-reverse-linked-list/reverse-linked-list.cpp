2c3516d3fffe57d4170f975d08e6a2b411f8b910
shivsins <shivjeetsingh3717@gmail.com>
2024-05-27 20:18:58 +0530

    Time: 8 ms (16.71%) | Memory: 11.9 MB (6.11%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,3 +1,11 @@
+// two ways to solve this problem 1. iterative 2. recursive
+//commented code is iterative
+//in recursive approach we are breaking the problem in smaller parts and when we reach the base case 
+//we return the head itself which will become our newHead.
+//after hitting the base case we come back to previous state and reverse the previous two links.
+//we always return the newHead which will remain the same once we hit the base condition
+//a liitle tricky but take a pen and paper in yu will be able to visualize it
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
