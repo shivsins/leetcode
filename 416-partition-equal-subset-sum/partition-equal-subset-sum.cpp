2a23fd6811a00f4926ce125d97ef2faabc123e13
shivsins <shivjeetsingh3717@gmail.com>
2025-06-15 21:55:00 +0530

    Time: 135 ms (48.01%) | Memory: 17.5 MB (52.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -9,18 +9,23 @@ public:
         if(sum%2!=0) return false;
         sum = sum/2;
         vector<vector<bool>> dp(n, vector<bool>(sum+1,false));
-        for(int i=0;i<n;i++){
-            dp[i][0] = true;
-        }
-        if(nums[0]<=sum) dp[0][nums[0]]=true;
+        vector<bool> prev(sum+1,false);
+        vector<bool> curr(sum+1,false);
+        prev[0]=true;
+        curr[0]=true;
+        // for(int i=0;i<n;i++){
+        //     dp[i][0] = true;
+        // }
+        if(nums[0]<=sum) prev[nums[0]]=true;
         for(int i=1;i<n;i++){
             for(int t=1;t<=sum;t++){
-                bool notTake = dp[i-1][t];
+                bool notTake = prev[t];
                 bool take = false;
-                if(nums[i]<=t) take=dp[i-1][t-nums[i]];
-                dp[i][t]=take||notTake;
+                if(nums[i]<=t) take=prev[t-nums[i]];
+                curr[t]=take||notTake;
             }
+            prev=curr;
         }
-        return dp[n-1][sum];
+        return curr[sum];
     }
 };
