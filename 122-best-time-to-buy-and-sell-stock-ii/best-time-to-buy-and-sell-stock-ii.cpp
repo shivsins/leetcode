df24ea62fdc63a690ebb6c5c5d69a6b3e57a9353
shivsins <shivjeetsingh3717@gmail.com>
2025-06-28 14:16:53 +0530

    Time: 7 ms (20.73%) | Memory: 22.5 MB (6.93%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int l=0, r=1, res=0;
+        // int l=0, r=1, res=0;
+        // int n = prices.size();
+        // if(n<=1) return 0;
+        // while(l<n && r<n){
+        //     while(l<n-1 && prices[l]>=prices[l+1]) l++;
+        //     r=l+1;
+        //     while(r<n-1 && prices[r]<=prices[r+1]) r++;
+        //     if(r<n && prices[r]>prices[l]) res+=prices[r]-prices[l];
+        //     l=r+1;
+        // }
+        // return res;
         int n = prices.size();
-        if(n<=1) return 0;
-        while(l<n && r<n){
-            while(l<n-1 && prices[l]>=prices[l+1]) l++;
-            r=l+1;
-            while(r<n-1 && prices[r]<=prices[r+1]) r++;
-            if(r<n && prices[r]>prices[l]) res+=prices[r]-prices[l];
-            l=r+1;
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return f(prices,0,prices.size(),1,dp);
+    }
+    int f(vector<int>& prices, int i, int n, int buy, vector<vector<int>> &dp){
+        if(i==n) return 0;
+        int profit=0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(buy){
+            dp[i][buy]=max(-prices[i]+f(prices, i+1,n,0,dp),f(prices,i+1,n,1,dp));
+        }else{
+            dp[i][buy]=max(prices[i]+f(prices,i+1,n,1,dp),f(prices,i+1,n,0,dp));
         }
-        return res;
+        return dp[i][buy];
     }
 };
