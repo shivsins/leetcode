19dd04e4c918527a75181433fdb123f6a4d7a45a
shivsins <shivjeetsingh3717@gmail.com>
2025-10-08 17:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (90.56%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
         int n = nums.size();
-        vector<int> res(n, -1);
-        vector<bool> found(n);
-        stack<pair<int,int>> s;
-        for(int i=0;i<n;i++){
-            found[i] = 0;
-        }
-        for(int i=0;i<n;i++){
-            while(!s.empty() && nums[i]>s.top().first) {
-                res[s.top().second] = nums[i];
-                found[s.top().second] = 1;
-                s.pop();
-            }
-            s.push({nums[i],i});
-        }
-        for(int i=0;i<n;i++){
-            while(!s.empty() && nums[i]>s.top().first) {
-                if(!found[s.top().second]){
-                    res[s.top().second] = nums[i];
+        vector<int> res(n,-1);
+        for(int i=2*n-1; i>=0; i--){
+            while(!st.empty() && nums[i%n]>=st.top()) st.pop();
+            if(!st.empty()){
+                if(i<n){
+                    res[i]=st.top();
                 }
-                found[s.top().second] = 1;
-                s.pop();
             }
-            s.push({nums[i],i});
+            st.push(nums[i%n]);
         }
         return res;
     }
