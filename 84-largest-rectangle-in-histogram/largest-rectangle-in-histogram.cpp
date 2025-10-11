c6d6038b1265b7e6361f23f23efbfeea3ef6bdc1
shivsins <shivjeetsingh3717@gmail.com>
2025-10-11 23:21:23 +0530

    Time: 29 ms (35.59%) | Memory: 79.9 MB (88.59%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,35 +2,57 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> pse(n,0);
-        vector<int> nse(n,0);
+        // vector<int> pse(n,0);
+        // vector<int> nse(n,0);
         stack<pair<int,int>> st;
+        // for(int i=0;i<n;i++){
+        //     while(!st.empty() && heights[i]<=st.top().second) {
+        //         st.pop();
+        //     }
+        //     if(!st.empty()){
+        //         pse[i]=st.top().first;
+        //     }else{
+        //         pse[i]=-1;
+        //     }
+        //     st.push({i,heights[i]});
+        // }
+        // stack<pair<int,int>> st1;
+        // for(int i=n-1;i>=0;i--){
+        //     while(!st1.empty() && heights[i]<=st1.top().second) {
+        //         st1.pop();
+        //     }
+        //     if(!st1.empty()){
+        //         nse[i]=st1.top().first;
+        //     }else{
+        //         nse[i]=n;
+        //     }
+        //     st1.push({i,heights[i]});
+        // }
+        int ans=0;
+        // for(int i=0;i<n;i++){
+        //     ans = max(ans,(nse[i]-pse[i]-1)*heights[i]);
+        // }
+        // return ans;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[i]<=st.top().second) {
+            while(!st.empty() && heights[i]<=st.top().second){
+                pair<int,int> temp = st.top();
                 st.pop();
-            }
-            if(!st.empty()){
-                pse[i]=st.top().first;
-            }else{
-                pse[i]=-1;
+                if(!st.empty()){
+                    ans=max(ans,(i-st.top().first-1)*temp.second);
+                }else{
+                    ans=max(ans,(i)*temp.second);
+                }
             }
             st.push({i,heights[i]});
         }
-        stack<pair<int,int>> st1;
-        for(int i=n-1;i>=0;i--){
-            while(!st1.empty() && heights[i]<=st1.top().second) {
-                st1.pop();
-            }
-            if(!st1.empty()){
-                nse[i]=st1.top().first;
+        while(!st.empty()){
+            pair<int,int> temp = st.top();
+            st.pop();
+            if(!st.empty()){
+                ans=max(ans,(n-st.top().first-1)*temp.second);
             }else{
-                nse[i]=n;
+                ans=max(ans,(n)*temp.second);
             }
-            st1.push({i,heights[i]});
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans = max(ans,(nse[i]-pse[i]-1)*heights[i]);
         }
         return ans;
     }
