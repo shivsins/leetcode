3da10033bba0b6f24038e1b922361714c7d4b3ef
shivsins <shivjeetsingh3717@gmail.com>
2025-11-16 19:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (17.61%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int ans = INT_MIN;
+        for(int i=0; i<n; i++){
+            sum = sum + nums[i];
+            ans = max(sum, ans);
+            if(sum<0) sum=0;
+        }
+        return ans;
+    }
+};
