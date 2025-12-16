e2b1852dbcae3432ef60d8f9799b5fdc3af3cc32
shivsins <shivjeetsingh3717@gmail.com>
2025-12-16 23:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (58.15%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,14 +4,21 @@ public:
         stack<int> st;
         int n = nums.size();
         vector<int> res(n,-1);
-        for(int i=2*n-1; i>=0; i--){
-            while(!st.empty() && nums[i%n]>=st.top()) st.pop();
-            if(!st.empty()){
-                if(i<n){
-                    res[i]=st.top();
-                }
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                res[st.top()]=nums[i];
+                st.pop();
             }
-            st.push(nums[i%n]);
+            st.push(i);
+        }
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                if(res[st.top()]==-1){
+                    res[st.top()]=nums[i];
+                }
+                st.pop();
+            }
+            st.push(i);
         }
         return res;
     }
