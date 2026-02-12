14aed473d133d67c1378f20c054bb6a43b56e185
shivsins <shivjeetsingh3717@gmail.com>
2026-02-12 23:56:25 +0530

    Time: 21 ms (71.03%) | Memory: 23.4 MB (90.44%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,20 +15,23 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) root;
-        if(root->val==p->val || root->val == q->val) return root;
-        bool lp = find(root->left,p);
-        bool rp = find(root->right,p);
-        bool lq = find(root->left,q);
-        bool rq = find(root->right,q);
-        if((lp && rq) || (rp && lq)) return root;
-        if(lp && lq) return lowestCommonAncestor(root->left,p,q);
-        else return lowestCommonAncestor(root->right,p,q);
+        // if(root==NULL) root;
+        // if(root->val==p->val || root->val == q->val) return root;
+        // bool lp = find(root->left,p);
+        // bool rp = find(root->right,p);
+        // bool lq = find(root->left,q);
+        // bool rq = find(root->right,q);
+        // if((lp && rq) || (rp && lq)) return root;
+        // if(lp && lq) return lowestCommonAncestor(root->left,p,q);
+        // else return lowestCommonAncestor(root->right,p,q);
+        if(p->val<root->val && q->val<root->val) return lowestCommonAncestor(root->left, p,q);
+        else if(p->val>root->val && q->val>root->val) return lowestCommonAncestor(root->right, p,q);
+        else return root;
     }
 
-    bool find(TreeNode* root, TreeNode* child){
-        if(root==NULL) return false;
-        if(root->val==child->val) return true;
-        return find(root->left, child) || find(root->right,child);
-    }
+    // bool find(TreeNode* root, TreeNode* child){
+    //     if(root==NULL) return false;
+    //     if(root->val==child->val) return true;
+    //     return find(root->left, child) || find(root->right,child);
+    // }
 };
