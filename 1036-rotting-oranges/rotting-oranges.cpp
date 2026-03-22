0e63c66d55c28c9164f52c2a9638f7493b183c32
shivsins <shivjeetsingh3717@gmail.com>
2026-03-22 20:49:20 +0530

    Time: 8 ms (6.53%) | Memory: 18.9 MB (5.02%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
+        int m =grid.size();
         int n = grid[0].size();
+        vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
         queue<vector<int>> q;
-        int total = 0;
+        int f=0;
+        int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2) q.push({i,j});
-                if(grid[i][j]==1) total++;
+                if(grid[i][j]==1) f++;
             }
         }
-        int rotten = 0;
-        int ans = 0;
-        bfs(grid, m, n, q, ans, total);
-        if(total>0) return -1;
-        else return ans;
-        // return ans;
-    }
-
-    void bfs(vector<vector<int>>& grid, int m, int n, queue<vector<int>> q, int &ans, int &total){
-        vector<vector<int>> dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        while(!q.empty() && total>0){
-            int qn = q.size();
-            for(int i=0;i<qn;i++){
+        if(f==0) return 0;
+        while(!q.empty()){
+            int len=q.size();
+            ans++;
+            for(int i=0;i<len;i++){
                 vector<int> curr = q.front();
                 q.pop();
-                for(vector<int> vec : dir){
-                    int x = curr[0] + vec[0];
-                    int y = curr[1] + vec[1];
+                for(vector<int> d : dir){
+                    int x = curr[0]+d[0];
+                    int y = curr[1]+d[1];
                     if(x>=0 && x<m && y>=0 && y<n){
-                        // vector<int> temp = {x,y};
-                        if(grid[x][y]==1) {
+                        if(grid[x][y]==1){
+                            f--;
+                            grid[x][y]=2;
                             q.push({x,y});
-                            grid[x][y] = 2;
-                            total--;
-                        }    
+                        }
                     }
                 }
             }
-            ans++;
+            
         }
+        if(f==0) return ans-1;
+        return -1;
     }
 };
