446fa0a7c8596ff2c7c455c1e542be19d1196f80
shivsins <shivjeetsingh3717@gmail.com>
2026-04-12 20:12:02 +0530

    Time: 19 ms (53.35%) | Memory: 46.6 MB (51.31%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> parent, size;
+
+    int findParent(int node){
+        if(parent[node]==node) return node;
+        else return parent[node]=findParent(parent[node]);
+    }
+
+    void unite(int u, int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if(pu==pv) return;
+        else if(size[u]<size[v]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i = 0; i < n; i++) parent[i] = i;
+        int extra=0;
+        for(auto c : connections){
+            int u = c[0], v = c[1];
+            int pu = findParent(u);
+            int pv = findParent(v);
+            if(pu==pv) extra++;
+            else{
+                unite(u,v);
+            }
+        }
+        int noc = 0;
+        for(int i=0; i<n; i++){
+            if(parent[i]==i){
+                noc++;
+            }
+        }
+         if(extra>=noc-1) return noc-1;
+         return -1;
+    }
+};
