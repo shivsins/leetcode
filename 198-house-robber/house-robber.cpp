6391b8efd5603256b547de9919b857563af0d2db
shivsins <shivjeetsingh3717@gmail.com>
2026-06-02 20:48:07 +0530

    Time: 3 ms (6.28%) | Memory: 10.8 MB (20.84%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,17 +2,27 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int rob=nums[i];
-            int notRob=INT_MIN;
-            if(i>1){
-                rob=nums[i]+dp[i-2];
-            }
-            notRob=dp[i-1];
-            dp[i]=max(rob, notRob);
-        }
-        return dp[n-1];
+        vector<int> dp(n,-1);
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int rob=nums[i];
+        //     int notRob=INT_MIN;
+        //     if(i>1){
+        //         rob=nums[i]+dp[i-2];
+        //     }
+        //     notRob=dp[i-1];
+        //     dp[i]=max(rob, notRob);
+        // }
+        // return dp[n-1];
+        return recur(n-1, dp, nums);
+    }
+
+    int recur(int ind, vector<int> &dp, vector<int>& nums){
+        if(ind==0) return nums[0]; 
+        if(dp[ind]!=-1) return dp[ind];
+        int notRob=recur(ind-1,dp,nums);
+        int rob=nums[ind];
+        if(ind>1) rob=nums[ind]+recur(ind-2,dp, nums);
+        return dp[ind] = max(notRob, rob);
     }
 };
