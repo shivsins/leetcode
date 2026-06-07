d7e2ea4315a8369b68e05c029cc581181c31408f
shivsins <shivjeetsingh3717@gmail.com>
2026-06-07 19:05:27 +0530

    Time: 5 ms (6.26%) | Memory: 16.8 MB (15.01%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,20 +4,18 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<long>> dp(m, vector<long>(n,0));
-        dp[m-1][n-1] = grid[m-1][n-1];
-        for(int i=m-2;i>=0;i--){
-            dp[i][n-1]=grid[i][n-1]+dp[i+1][n-1];
-        }
-        for(int i=n-2;i>=0;i--){
-            dp[m-1][i]=grid[m-1][i]+dp[m-1][i+1];
-        }
-        for(int i=m-2; i>=0;i--){
-            for(int j=n-2; j>=0; j--){
-                dp[i][j]=grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
-                cout<<dp[i][j]<<" ";
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else{
+                    int left=INT_MAX;
+                    int up=INT_MAX;
+                    if(i>0) up=dp[i-1][j]+grid[i][j];
+                    if(j>0) left=dp[i][j-1]+grid[i][j];
+                    dp[i][j]=min(up,left);
+                }
             }
-            cout<<endl;
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
 };
