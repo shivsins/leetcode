674085f4e332261003cd6a9aab6b1497d351f70b
shivsins <shivjeetsingh3717@gmail.com>
2026-06-18 19:21:23 +0530

    Time: 154 ms (32.25%) | Memory: 17.5 MB (58.79%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -3,29 +3,21 @@ public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum =0;
-        for(int e : nums){
-            sum+=e;
-        }
+        for(int i=0;i<n;i++) sum+=nums[i];
         if(sum%2!=0) return false;
-        sum = sum/2;
-        vector<vector<bool>> dp(n, vector<bool>(sum+1,false));
-        vector<bool> prev(sum+1,false);
-        vector<bool> curr(sum+1,false);
-        prev[0]=true;
-        curr[0]=true;
-        // for(int i=0;i<n;i++){
-        //     dp[i][0] = true;
-        // }
-        if(nums[0]<=sum) prev[nums[0]]=true;
+        int k=sum/2;
+        vector<vector<bool>> dp(n, vector<bool>(k+1,0));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(nums[0]<k) dp[0][nums[0]]=true;
         for(int i=1;i<n;i++){
-            for(int t=1;t<=sum;t++){
-                bool notTake = prev[t];
-                bool take = false;
-                if(nums[i]<=t) take=prev[t-nums[i]];
-                curr[t]=take||notTake;
+            for(int target=1;target<=k;target++){
+                int notTake=dp[i-1][target];
+                int take=false;
+                if(nums[i]<=target) take=dp[i-1][target-nums[i]];
+                dp[i][target]=take||notTake;
             }
-            prev=curr;
         }
-        return curr[sum];
+        return dp[n-1][k];
     }
+
 };
