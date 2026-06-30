b62260111bb3e7b3b715aabcbb5823273931363a
shivsins <shivjeetsingh3717@gmail.com>
2026-06-30 20:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.96%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         int sum = 0;
         int ans = INT_MIN;
-        for(int i=0; i<n; i++){
-            sum = sum + nums[i];
-            ans = max(sum, ans);
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            ans=max(sum,ans);
             if(sum<0) sum=0;
         }
         return ans;
